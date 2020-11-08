36deff515353505e67b7db1878d6d4ae25e70936
Poorni Amaratunga <poorniamarathunga1997@gmail.com>
2020-11-09 00:52:16 +0530

    Update manage customer form

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,20 +1,37 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class DashboardFormController {
     public AnchorPane root;
 
     public void btn_ManageCustomers_OnAction(ActionEvent actionEvent) {
+        try {
+            setUI("ManageCustomerForm");
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR,"No Such Page to load");
+            e.printStackTrace();
+        }
     }
 
     public void btn_ManageItems_OnAction(ActionEvent actionEvent) {
     }
 
-    public void btn_PalceOrder_OnAction(ActionEvent actionEvent) {
+    public void btn_PlaceOrder_OnAction(ActionEvent actionEvent) {
     }
 
     public void btn_OrderDetails_OnAction(ActionEvent actionEvent) {
+    }
+    public void setUI(String location) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/"+location+".fxml"))));
     }
 }
--- src/controller/ManageCustomerFormController.java
+++ src/controller/ManageCustomerFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class ManageCustomerFormController {
+    public AnchorPane root;
+    public JFXTextField txtCustomerId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public TableView tblCustomer;
+    public TableColumn colCustomerId;
+    public TableColumn colFullName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colOperate;
+    public JFXTextField txtAddress1;
+
+    public void btnAddCustomer_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateCustomer_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearch_OnAction(ActionEvent actionEvent) {
+    }
 }
